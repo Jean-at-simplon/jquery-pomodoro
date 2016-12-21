bd918df7c5588be02a7866b212c90dc833fdd44d
Jean-at-simplon <jeanlefrancois.simplon@gmail.com>
2016-12-21 16:48:14 +0100

    Pomodoro version 1.2

--- js/script.js
+++ js/script.js
@@ -5,55 +5,77 @@
     var interval;
     var compteur = 15;
     var pause = 0;
+    var etat = 'stop';
 
 //initialisation du timer//
-    function commencerCompteur() {
-        interval = setInterval(enleverSec, 1000);
+    function etatPlay() {
+        $('#task').attr('disabled','disabled');
+        if (etat != 'play'){
+            etat = 'play';
+            interval = window.setInterval(function (){
+                compteur--;
+                affichage(compteur);
+            }, 1000);
+            
+        }
     }
 
 //calcul du temps et affichage//
-    function enleverSec() {
-        var minute = 0;
-        var seconde = 0;
-        compteur--;
-        if (compteur == 0) {
-            stop();
+    function affichage(temps) {
+       
+        if (temps == 0) {
+            etatStop();
         }
 
-        minute = Math.floor(compteur / 60);
+        var minute = Math.floor(temps / 60);
         if (minute < 10) {
             minute = '0' + minute;
         }
 
-        seconde = Math.ceil(compteur % 60);
+        var seconde = Math.ceil(temps % 60);
         if (seconde < 10) {
             seconde = '0' + seconde;
         }
        
         $('#timer').text(minute + ":" + seconde);
     }
+//pause du timer//
+    function etatPause() {
+        etat = 'pause';
+        window.clearInterval(interval);
+        $('#timer').text('pause');
+    }
 
 //fin du timer et actualisation des variables//
-    function stop() {
-        clearInterval(interval);
+    function etatStop() {
+        etat = 'stop';
+        window.clearInterval(interval);
+       
         pause++;
 
         if (pause % 7 == 0) {
-            var input = $("#task").val();
-            $('.list-group').prepend('<li class="list-group-item"><s>' + input + '</s></li>');
             compteur = 10;
             $('#timer').text('00:10');
+            affichageDone();
         } else if (pause % 2 == 0) {
             compteur = 15;
             $('#timer').text('00:15');
+            $('#pause').text('Doing');
         } else {
             compteur = 5;
             $('#timer').text('00:05');
-            var input = $("#task").val();
-            $('.list-group').prepend('<li class="list-group-item"><s>' + input + '</s></li>');
+            affichageDone();
         }
 
-        commencerCompteur();
+        etatPlay();
+    }
+
+//affichage en fin de compteur//
+    function affichageDone() {
+         var input = $("#task").val();
+         $('.list-group').prepend('<li class="list-group-item"><s>' + input + '</s></li>');
+         $('#pause').text('En pause');
+         $('#task').removeAttr('disabled');
     }
 /*********** FIN TIMER ************/
 
@@ -63,21 +85,17 @@
 $(document).ready(function () {
 
 //lancement du timer//
-    $('#play').click(function () {   
-            $('#timer').text('00:15');
-            commencerCompteur();
-        }),
+    $('#play').click(etatPlay);
 
 //mise en pause du timer//
-        $('#pause').click(function () {     
-            clearInterval(interval);
-            $('#timer').text('pause');
-        }),
+    $('#pause').click(etatPause);
 
 //arret du timer et remise à zéro//
-        $('#stop').click(function () {      
-            clearInterval(interval);
-            $('#timer').text('Tache Annulée');
-            compteur = 15;
-        })
+    $('#stop').click(etatStop);
+
+        $('input').keypress(function (event) {
+            if (keycode (event.which) == 13) {
+            console.log('Enter');
+    }
+})
 });
